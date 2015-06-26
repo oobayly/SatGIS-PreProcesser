2c2571e0fe6de53a7f7abf06d9524617c1c99212
John Bayly <john.bayly@tipstrade.net>
2015-06-26 10:15:25 +0100

    Initial commit

---
+++ SatGIS PreProcesser/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace SatGIS_PreProcesser
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.butProcess = new System.Windows.Forms.Button();
+            this.folderSelector = new System.Windows.Forms.FolderBrowserDialog();
+            this.lblProcessing = new System.Windows.Forms.Label();
+            this.listProcessed = new System.Windows.Forms.ListBox();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // butProcess
+            // 
+            this.butProcess.Location = new System.Drawing.Point(3, 3);
+            this.butProcess.Name = "butProcess";
+            this.butProcess.Size = new System.Drawing.Size(75, 23);
+            this.butProcess.TabIndex = 0;
+            this.butProcess.Text = "Process Directory";
+            this.butProcess.UseVisualStyleBackColor = true;
+            this.butProcess.Click += new System.EventHandler(this.butProcess_Click);
+            // 
+            // folderSelector
+            // 
+            this.folderSelector.Description = "Select the folder containing the KMZ files to process";
+            this.folderSelector.ShowNewFolderButton = false;
+            // 
+            // lblProcessing
+            // 
+            this.lblProcessing.AutoSize = true;
+            this.lblProcessing.Location = new System.Drawing.Point(3, 29);
+            this.lblProcessing.Name = "lblProcessing";
+            this.lblProcessing.Size = new System.Drawing.Size(16, 13);
+            this.lblProcessing.TabIndex = 1;
+            this.lblProcessing.Text = "...";
+            // 
+            // listProcessed
+            // 
+            this.listProcessed.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listProcessed.FormattingEnabled = true;
+            this.listProcessed.IntegralHeight = false;
+            this.listProcessed.Location = new System.Drawing.Point(3, 45);
+            this.listProcessed.Name = "listProcessed";
+            this.listProcessed.Size = new System.Drawing.Size(291, 292);
+            this.listProcessed.TabIndex = 2;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.butProcess, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.listProcessed, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.lblProcessing, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(297, 315);
+            this.tableLayoutPanel1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(297, 315);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "SatGIS PreProcessor";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button butProcess;
+        private System.Windows.Forms.FolderBrowserDialog folderSelector;
+        private System.Windows.Forms.Label lblProcessing;
+        private System.Windows.Forms.ListBox listProcessed;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+    }
+}
+
---
+++ SatGIS PreProcesser/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Xml;
+using ICSharpCode.SharpZipLib.Zip;
+
+namespace SatGIS_PreProcesser
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void butProcess_Click(object sender, EventArgs e)
+        {
+            DirectoryInfo dir = null;
+            if (!string.IsNullOrEmpty(Properties.Settings.Default.LastPath))
+            {
+                dir = new DirectoryInfo(Properties.Settings.Default.LastPath);
+            }
+            if (dir == null || !dir.Exists)
+                dir = new DirectoryInfo(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments));
+
+            folderSelector.SelectedPath = dir.FullName;
+            if (folderSelector.ShowDialog(this) == System.Windows.Forms.DialogResult.Cancel)
+                return;
+
+            Properties.Settings.Default.LastPath = folderSelector.SelectedPath;
+            Properties.Settings.Default.Save();
+
+            try
+            {
+                ProcessPath(new DirectoryInfo(folderSelector.SelectedPath));
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(this, ex.Message + Environment.NewLine + ex.StackTrace, this.Text,
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        public void ProcessPath(DirectoryInfo src)
+        {
+            DirectoryInfo trg = new DirectoryInfo(src + @"\Processed\");
+            if (!trg.Exists)
+                trg.Create();
+
+            FileInfo[] files = src.GetFiles("*.kmz");
+            lblProcessing.Text = string.Format("Processing {0} files in '{1}'.", files.Length, src);
+            foreach (FileInfo fi in files)
+            {
+                ProcessFile(fi, trg);
+            }
+        }
+
+        public void ProcessFile(FileInfo fi, DirectoryInfo trg)
+        {
+            using (FileStream fs = fi.OpenRead())
+            {
+                using (ZipFile zip = new ZipFile(fs))
+                {
+                    for (int i = 0; i < zip.Count; i++)
+                    {
+                        ZipEntry entry = zip[i];
+                        if (entry.Name.EndsWith(".kml"))
+                        {
+                            ProcessEntry(zip, entry, trg);
+                        }
+                    }
+                }
+            }
+        }
+
+        public void ProcessEntry(ZipFile zip, ZipEntry entry, DirectoryInfo dir)
+        {
+            FileInfo trgFile = new FileInfo(dir.FullName + "\\" + entry.Name);
+
+            //byte[] buff = new byte[4096];
+            string kmlText;
+            using (Stream src = zip.GetInputStream(entry))
+            {
+                /*			using (FileStream trg = new FileStream(trgFile.FullName, FileMode.Create, FileAccess.Write)) {
+                                int read;
+                                do {
+                                    read = src.Read(buff, 0, buff.Length);
+                                    trg.Write(buff, 0, read);
+                                } while (read > 0);
+                            }*/
+                using (StreamReader reader = new StreamReader(src))
+                {
+                    kmlText = reader.ReadToEnd();
+                }
+            }
+
+            /* avmap namespace is incorrectly references as xmlns:prefix="avmap"
+             * it should look like: xmlns:avmap="avmap", so do a simple replace
+             */
+            kmlText = kmlText.Replace("xmlns:prefix=\"avmap\"", "xmlns:avmap=\"avmap\"");
+
+            // Make sure the document can be opened
+            XmlDocument doc = new XmlDocument();
+            doc.LoadXml(kmlText);
+
+            // And save
+            doc.Save(trgFile.FullName);
+            listProcessed.Items.Add(string.Format("Processed {0}", trgFile.Name));
+        }
+    }
+}
