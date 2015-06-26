19d0cd6ec1282308ac985c0edbbfb2ae68e559dc
John Bayly <john.bayly@tipstrade.net>
2015-06-26 10:17:42 +0100

    Fix formatting

--- SatGIS PreProcesser/Program.cs
+++ SatGIS PreProcesser/Program.cs
@@ -3,19 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms;
 
-namespace SatGIS_PreProcesser
-{
-    static class Program
-    {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        static void Main()
-        {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
-        }
+namespace SatGIS_PreProcesser {
+  static class Program {
+    /// <summary>
+    /// The main entry point for the application.
+    /// </summary>
+    [STAThread]
+    static void Main() {
+      Application.EnableVisualStyles();
+      Application.SetCompatibleTextRenderingDefault(false);
+      Application.Run(new Form1());
     }
+  }
 }
